17b1670434e335803c5b9e7f8f8af07342d73103
Viktor <malyshkin.viktor@icloud.com>
2021-02-04 01:31:17 +0300

    optimization routes

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {StoreModule} from '@ngrx/store';
 import {TODO_REDUCER_NODE, todoReducer} from './store/todo.reducer';
-import { TodoComponent } from './page/todo/todo.component';
+import {TodoComponent} from './page/todo/todo.component';
 import {RouterModule} from '@angular/router';
+import {todoRoutes} from './routes/todo.routes';
 
 
 @NgModule({
@@ -11,10 +12,7 @@ import {RouterModule} from '@angular/router';
   imports: [
     CommonModule,
     StoreModule.forFeature(TODO_REDUCER_NODE, todoReducer),
-    RouterModule.forChild([{
-      path: '',
-      component: TodoComponent
-    }])
+    RouterModule.forChild(todoRoutes),
   ],
 })
 export class TodoModule {
