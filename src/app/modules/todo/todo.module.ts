0d57f1f1d80d2cf6b929b79f7b9432a1404403b9
Viktor <malyshkin.viktor@icloud.com>
2021-02-04 00:57:37 +0300

    add route for Todo

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {StoreModule} from '@ngrx/store';
 import {TODO_REDUCER_NODE, todoReducer} from './store/todo.reducer';
+import { TodoComponent } from './page/todo/todo.component';
+import {RouterModule} from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [TodoComponent],
   imports: [
     CommonModule,
     StoreModule.forFeature(TODO_REDUCER_NODE, todoReducer),
+    RouterModule.forChild([{
+      path: '',
+      component: TodoComponent
+    }])
   ],
 })
 export class TodoModule {
