de015ff1935a9ac75474c8e11f7d9daa1f526d3c
Viktor <malyshkin.viktor@icloud.com>
2021-02-04 02:12:53 +0300

    add TodoWidget & TodoCreateFormUI components

--- src/app/modules/todo/todo.module.ts
+++ src/app/modules/todo/todo.module.ts
@@ -4,11 +4,13 @@ import { RouterModule } from '@angular/router'
 import { StoreModule } from '@ngrx/store'
 import { TodoComponent } from './page/todo/todo.component'
 import { todoRoutes } from './routes/todo.routes'
-import { TODO_REDUCER_NODE, todoReducer } from './store/todo.reducer'
+import { TODO_REDUCER_NODE, todoReducer } from './store/todo.reducer';
+import { TodoWidgetComponent } from './widget/todo-widget/todo-widget.component';
+import { TodoCreateFormUiComponent } from './ui/todo-create-form-ui/todo-create-form-ui.component'
 
 
 @NgModule({
-  declarations: [TodoComponent],
+  declarations: [TodoComponent, TodoWidgetComponent, TodoCreateFormUiComponent],
   imports: [
     CommonModule,
     StoreModule.forFeature(TODO_REDUCER_NODE, todoReducer),
---
+++ src/app/modules/todo/ui/todo-create-form-ui/todo-create-form-ui.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-create-form-ui',
+  templateUrl: './todo-create-form-ui.component.html',
+  styleUrls: ['./todo-create-form-ui.component.css']
+})
+export class TodoCreateFormUiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
