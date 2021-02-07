10ca8642dd6aaea2a0007ad3c78601929bbe6448
Viktor <malyshkin.viktor@icloud.com>
2021-02-07 23:57:33 +0300

    add routes

--- src/app/modules/exchange-rates-nbrb/exchange-rates-nbrb.module.ts
+++ src/app/modules/exchange-rates-nbrb/exchange-rates-nbrb.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
+import { RouterModule } from '@angular/router'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreModule } from '@ngrx/store'
+import { exchangeRatesNbrbRoutes } from './routes/exchange-rates-nbrb.routes'
 import { ExchangeRatesNbrbEffects } from './store/effects/exchange-rates-nbrb.effects'
-import * as fromExchangeRatesNbrb from './store/reducers/exchange-rates-nbrb.reducer'
+import * as fromExchangeRatesNbrb from './store/reducers/exchange-rates-nbrb.reducer';
 import { ExchangeRatesNbrbPageComponent } from './pages/exchange-rates-nbrb-page/exchange-rates-nbrb-page.component'
 
 @NgModule({
@@ -12,6 +14,7 @@ import { ExchangeRatesNbrbPageComponent } from './pages/exchange-rates-nbrb-page
     CommonModule,
     StoreModule.forFeature(fromExchangeRatesNbrb.exchangeRatesNbrbFeatureKey, fromExchangeRatesNbrb.reducer),
     EffectsModule.forFeature([ExchangeRatesNbrbEffects]),
+    RouterModule.forChild(exchangeRatesNbrbRoutes),
   ],
 })
 
---
+++ src/app/modules/exchange-rates-nbrb/routes/exchange-rates-nbrb.routes.ts
@@ -0,0 +1,10 @@
+import { Route } from '@angular/router'
+import { ExchangeRatesNbrbPageComponent } from '../pages/exchange-rates-nbrb-page/exchange-rates-nbrb-page.component'
+// import { PreciousMetalsPageComponent } from '../pages/precious-metals-page/precious-metals-page.component'
+
+export const exchangeRatesNbrbRoutes: Route[] = [
+  {
+    path: '',
+    component: ExchangeRatesNbrbPageComponent,
+  },
+]
