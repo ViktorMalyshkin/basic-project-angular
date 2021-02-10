50f36b2462141969c601cfca8f6dbba9bf3abfdd
Viktor <malyshkin.viktor@icloud.com>
2021-02-10 22:06:03 +0300

    update app.module

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,20 +1,9 @@
-import {
-  ActionReducer,
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector,
-  MetaReducer
-} from '@ngrx/store';
-import { environment } from '../../../environments/environment';
+import { ActionReducerMap } from '@ngrx/store'
 
 
-export interface State {
+export interface State {}
 
-}
-
-export const reducers: ActionReducerMap<State> = {
-
-};
+export const reducers: ActionReducerMap<State> = {}
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+// export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
