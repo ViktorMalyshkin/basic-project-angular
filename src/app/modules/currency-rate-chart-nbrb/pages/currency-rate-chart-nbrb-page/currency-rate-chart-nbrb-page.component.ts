1cc0bb551a6b292e6aa6cace89423a5a1632a843
Viktor <malyshkin.viktor@icloud.com>
2021-03-01 19:56:13 +0300

    data change subscriptions in the store

--- src/app/modules/currency-rate-chart-nbrb/pages/currency-rate-chart-nbrb-page/currency-rate-chart-nbrb-page.component.ts
+++ src/app/modules/currency-rate-chart-nbrb/pages/currency-rate-chart-nbrb-page/currency-rate-chart-nbrb-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { Store } from '@ngrx/store'
+import { select, Store } from '@ngrx/store'
 import { Observable } from 'rxjs'
 import { environment } from '../../../../../environments/environment'
 import { GetCurrencies } from '../../store/actions/currency.actions'
@@ -15,35 +15,22 @@ import { selectDynamicsState } from '../../store/selectors/dynamics.selectors'
   styleUrls: ['./currency-rate-chart-nbrb-page.component.scss'],
 })
 export class CurrencyRateChartNbrbPageComponent implements OnInit {
-  dynamics$: Observable<DynamicsState>
-  currencies$: Observable<CurrenciesState>
+  dynamics$: Observable<DynamicsState> = this.store.select(selectDynamicsState)
+  currencies$: Observable<CurrenciesState> = this.store.pipe(select(selectCurrencyState))
   initialCurrencyChart: any
 
   constructor( private store: Store ) {
     this.initialCurrencyChart = environment.initial_currency_chart
-  }
-
-  ngOnInit(): void {
-    const receivingDynamics = this._receivingFromServerDynamics()
-    const receivingCurrencies = this._receivingFromServerCurrencies()
-    Promise.all([receivingDynamics, receivingCurrencies]).then(() => {
-      this.dynamics$ = this.store.select(selectDynamicsState)
-      this.currencies$ = this.store.select(selectCurrencyState)
-    })
-  }
-
-  private async _receivingFromServerDynamics(): Promise<void> {
-    await this.store.dispatch(new GetCurrencies())
-  }
-
-  private async _receivingFromServerCurrencies(): Promise<void> {
-    await this.store.dispatch(new GetDynamics(
+    this.store.dispatch(new GetCurrencies())
+    this.store.dispatch(new GetDynamics(
       {
         currency: this.initialCurrencyChart.id.toString(),
         startDate: this.initialCurrencyChart.date_start,
         endDate: this.initialCurrencyChart.date_end,
       }))
   }
+
+  ngOnInit(): void {}
 
   paramsEvent( $event: any ): void {
     this.store.dispatch(new GetDynamics($event.value))
--- src/app/modules/currency-rate-chart-nbrb/services/currency.service.ts
+++ src/app/modules/currency-rate-chart-nbrb/services/currency.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable, of } from 'rxjs'
+import { Observable } from 'rxjs'
 import { environment } from '../../../../environments/environment'
 import { serializeResponseCurrencies } from '../helpers/serialize-response-currency.helper'
 import { CurrencyModel } from '../models/currency.model'
+import { ResponseCurrencyModel } from '../models/response/response-currency.model'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyService {
-
-  constructor( private _http: HttpClient ) {}
+  constructor( private _http: HttpClient ) {
+  }
 
   public getCurrencies(): Observable<CurrencyModel[]> {
-      return this._http.get<CurrencyModel[]>(`${ environment.api_url }/exrates/currencies`).pipe(serializeResponseCurrencies)
+    return this._http.get<ResponseCurrencyModel[]>(`${ environment.api_url }/exrates/currencies`).pipe(serializeResponseCurrencies)
   }
 }
 
